b59e666210ebac2c3fa391c3c41d08c3ad23c143
Ansari Bilal <m.bilal8492@gmail.com>
2020-05-20 17:12:45 +0530

    Car

---
+++ Script.js
@@ -0,0 +1,13 @@
+function checkAge() {
+  var age = document.getElementById("age").value;
+  if (age < 18) {
+    document.getElementById("result").innerText =
+      "sorry, you are too young to drive this car. powering off";
+  } else if (age == 18) {
+    document.getElementById("result").innerText =
+      "Congratulations on your first year of driving. Enjoy the ride!";
+  } else {
+    document.getElementById("result").innerText =
+      "Powering On. Enjoy the ride!";
+  }
+}
